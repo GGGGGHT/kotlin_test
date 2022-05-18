f56c9016c3438e685afc9f1a9712c01d411bd1d2
ggggght <wz9712203617@gmail.com>
2022-05-18 19:29:21 +0800

    feat: kotlin access

---
+++ src/jvmMain/kotlin/com.ggggght.application/access.kt
@@ -0,0 +1,10 @@
+package com.ggggght.application
+
+
+// Kotlin 访问修辞符
+// https://www.kotlincn.net/docs/reference/access-modifiers.html
+//           Kotlin
+// public    默认修辞符全局可见
+// private   当前文件
+// protected 当前类 和子类
+// internal  当前模块
