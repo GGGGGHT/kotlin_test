e1a641772be4f86986c04e60b2ee567e3a200d13
ggggght <wz9712203617@gmail.com>
2022-05-23 17:41:58 +0800

    feat: kotlin lambda

--- src/jvmMain/kotlin/com.ggggght.application/collection.kt
+++ src/jvmMain/kotlin/com.ggggght.application/collection.kt
@@ -2,12 +2,12 @@ package com.ggggght.application
 
 data class Student(val name: String, val age: Int, val sex: String, val score: Int)
 
-val jilen = Student("Jilen",30,"m",85)
-val shaw = Student("Shaw",40,"m",90)
-val yison = Student("Yison",30,"m",59)
-val jack = Student("Jack",30,"m",70)
-val lisa = Student("Lisa",25,"f",88)
-val pan = Student("Pan",36,"f",55)
+val jilen = Student("Jilen", 30, "m", 85)
+val shaw = Student("Shaw", 40, "m", 90)
+val yison = Student("Yison", 30, "m", 59)
+val jack = Student("Jack", 30, "m", 70)
+val lisa = Student("Lisa", 25, "f", 88)
+val pan = Student("Pan", 36, "f", 55)
 var t = 10
 
 val students = listOf(jilen, shaw, yison, jack, lisa, pan, null)
@@ -15,3 +15,10 @@ val students = listOf(jilen, shaw, yison, jack, lisa, pan, null)
 val mstudents = students.filter { it?.sex == "m" }
 val fstudents = students.filterNot { it?.sex == "m" }
 val notNull = students.filterNotNull()
+
+val countMStudent = students.filter { it?.sex == "m" }.size
+val countMStudent1 = students.count { it?.sex == "m" }
+
+val scoreTotal = students.sumOf { it?.score ?: 0 }
+
+val sum = students.fold(0) { accumulator, student -> (accumulator + (student?.score ?: 0)) }
