4ef9bab04962d4c066b46d7b2ffc7ff27d02f8e2
GGGGGHT <wz9712203617@gmail.com>
2022-05-26 22:09:08 +0800

    feat: GOF23

---
+++ src/jvmMain/kotlin/com/ggggght/application/gof/factory.kt
@@ -0,0 +1,35 @@
+package com.ggggght.application.gof
+
+interface Computer {
+    val cpu: String
+}
+
+class PC(override val cpu: String = "Core") : Computer
+class Server(override val cpu: String = "Xeon") : Computer
+
+enum class ComputerType{
+    PC,Server
+}
+
+object ComputerFactory {
+    fun produce(type: ComputerType): Computer {
+        return when (type) {
+            ComputerType.PC -> PC("AMD")
+            ComputerType.Server -> Server()
+        }
+    }
+
+    operator fun invoke(type: ComputerType) : Computer {
+        return when (type) {
+            ComputerType.PC -> PC("AMD")
+            ComputerType.Server -> Server()
+        }
+    }
+}
+
+
+val comp = ComputerFactory(ComputerType.PC)
+
+fun main() {
+    println(comp.cpu)
+}
