9c0700ed4d1f62406cf0a851aedd7ba19f1feeed
GGGGGHT <wz9712203617@gmail.com>
2022-06-03 22:49:14 +0800

    Update newThread.kt

--- src/jvmMain/kotlin/com/ggggght/application/concurrent/newThread.kt
+++ src/jvmMain/kotlin/com/ggggght/application/concurrent/newThread.kt
@@ -2,6 +2,7 @@ package com.ggggght.application.concurrent
 
 import kotlin.concurrent.thread
 
+// use conutdownlatch
 fun main(args: Array<String>) {
   thread {
     println(Thread.currentThread().name)
