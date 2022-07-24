75e0f688c5c4b3e82e75f089cbd1aa751d252c2b
GGGGGHT <wz9712203617@gmail.com>
2022-07-24 23:03:47 +0800

    Update generic.kt

--- src/jvmMain/kotlin/com.ggggght.application/generic.kt
+++ src/jvmMain/kotlin/com.ggggght.application/generic.kt
@@ -19,4 +19,23 @@ fun main(args: Array<String>) {
   val l: SmartList<String> = arrayListOf("one", "two") as SmartList<String>
   println(list.find("one"))
   println(list.find("tow").isNullOrEmpty())
+  
+  fun printAll(vararg messages: String) {                            // 1
+        for (m in messages) println(m)
+    }
+    printAll("Hello", "Hallo", "Salut", "Hola", "你好")                 // 2
+
+    fun printAllWithPrefix(vararg messages: String, prefix: String) {  // 3
+        for (m in messages) println(prefix + m)
+    }
+    printAllWithPrefix(
+        "Hello", "Hallo", "Salut", "Hola", "你好",
+        prefix = "Greeting: "                                          // 4
+    )
+
+    fun log(vararg entries: String) {
+        printAll(*entries)                                             // 5
+    }
+    
+    log("Hello", "Hallo", "Salut", "Hola", "你好") 
 }
